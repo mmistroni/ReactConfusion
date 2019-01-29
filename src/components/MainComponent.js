fa2eaa98bd540d97394a67eb2636dc071991da9f
mmistroni <mmistroni@gmail.com>
2019-01-29 22:12:38 +0000

    Assignmetn 2/2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,6 +6,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from  './HomeComponent';
 import Contact from  './ContactComponent';
+import About from './AboutComponent';
 import {DISHES} from '../shared/dishes.js';
 import {PROMOTIONS} from '../shared/promotions.js';
 import {COMMENTS} from '../shared/comments.js';
@@ -43,6 +44,7 @@ class MainComponent extends Component {
                     <Route path="/home" component={HomePage}/>
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
                     <Route exact path="/contactus" component={Contact}/>
+                    <Route exact path="/aboutus" component= {() => <About leaders={this.state.leaders}/>}/>    
                     <Redirect to="/home" />
                 </Switch>
                 <Footer/>
