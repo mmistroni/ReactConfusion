fb2e7d24a9b27e9a961bc2821d110e2ba626ebe1
mmistroni <mmistroni@gmail.com>
2019-02-05 22:34:24 +0000

    Additional modifications

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,27 +1,43 @@
 import React, {Component} from 'react';
 import {Media} from'reactstrap';
-import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap'
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import DishComments from './DishCommentsComponent';
     
-    function DishDetail(props) {
+
+    function RenderDish({dish}) {
+        return (
+            <div key="details" className="col-12 col-md-5 m-1"> 
+            <Card>
+                <CardImg width="50%" src={dish.image} alt={dish.name}/>
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+        );
+    }
+
+    function RenderComments({dish}) {
+        return(
+            <div key="comments" className="col-12 col-md-5 m-1"> 
+                <DishComments dish={dish}/>
+            </div>
+        );
+    }
+
+
+    const DishDetail = (props) => {
         
         if (props.dish != null) {
-        
             return (
                     <div className="container">
                         <div className="row">
-                            <div key="details" className="col-12 col-md-5 m-1"> 
-                                <Card>
-                                    <CardImg width="50%" src={props.dish.image} alt={props.dish.name}/>
-                                    <CardBody>
-                                        <CardTitle>{props.dish.name}</CardTitle>
-                                        <CardText>{props.dish.description}</CardText>
-                                    </CardBody>
-                                </Card>
-                            </div>
-                            <div key="comments" className="col-12 col-md-5 m-1"> 
-                                <DishComments dish={props.dish}/>
-                            </div>
+                            <RenderDish dish={props.dish}/>
+                            <RenderComments dish={props.dish}/>
+                            
                         </div>
                     </div>
             );
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,17 +1,20 @@
 import React, {Component} from 'react';
 import {Media} from'reactstrap';
 import DishDetail  from './DishDetailComponent'
-import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap'
+import { Card, CardImg, CardImgOverlay,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-    function RenderMenuItem({dish, onClick}) {
-        
-        return (<Card key={dish.id}
-                      onClick={() => onClick(dish.id)}>
-                      <CardImg width="100%" src={dish.image} alt={dish.name} />
-                      <CardImgOverlay>
-                          <CardTitle>{dish.name}</CardTitle>
-                      </CardImgOverlay>
-                    </Card>
+    function RenderMenuItem ({dish, onClick}) {
+        return (
+            <Card>
+                <Link to={`/menu/${dish.id}`} >
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Link>
+            </Card>
         );
     }
         
@@ -25,10 +28,20 @@ import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reac
                 </div>
             );
         });
-        return ( 
+        return (
             <div className="container">
                 <div className="row">
-                        {menu}
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    {menu}
                 </div>
             </div>
         );    
