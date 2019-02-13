88f12539146fd1c8aa15db68713b86ca615abe17
mmistroni <mmistroni@gmail.com>
2019-02-13 22:07:06 +0000

    added form

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -35,11 +35,24 @@ import DishComments from './DishCommentsComponent';
             return (
                     <div className="container">
                         <div className="row">
-                            <RenderDish dish={props.dish}/>
-                            <RenderComments dish={props.dish}/>
-                            
+                            <Breadcrumb>
+                                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                            </Breadcrumb>
+                            <div className="col-12">
+                                <h3>{props.dish.name}</h3>
+                                <hr />
+                            </div>                
                         </div>
-                    </div>
+                        <div className="row">
+                            <div className="col-12 col-md-5 m-1">
+                                <RenderDish dish={props.dish}/>
+                            </div>
+                            <div className="col-12 col-md-5 m-1">
+                                <RenderComments dish={props.dish}/>
+                            </div>
+                        </div>
+                </div>
             );
         } else {
             return (
